bb3e1c89769c6cfab597b95260fd15ebafde43f4
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 16:46:21 -0300

    Created mapper launch entity to domain

---
+++ app/src/main/java/com/mvclopes/spacexlaunches/data/mapper/LaunchEntityToDomain.kt
@@ -0,0 +1,56 @@
+package com.mvclopes.spacexlaunches.data.mapper
+
+import com.mvclopes.spacexlaunches.data.datasource.local.model.LaunchEntity
+import com.mvclopes.spacexlaunches.data.datasource.local.model.LinksEntity
+import com.mvclopes.spacexlaunches.data.datasource.local.model.RocketEntity
+import com.mvclopes.spacexlaunches.domain.model.Launch
+import com.mvclopes.spacexlaunches.domain.model.Links
+import com.mvclopes.spacexlaunches.domain.model.Rocket
+
+fun List<LaunchEntity>.toDomain(): List<Launch> = this.map { it.toDomain() }
+
+fun List<Launch>.toEntity(): List<LaunchEntity> = this.map { it.toEntity() }
+
+fun Launch.toEntity() = LaunchEntity(
+    flightNumber = flightNumber,
+    missionName = missionName,
+    launchYear = launchYear,
+    rocket = rocket.toEntity(),
+    launchSuccess = launchSuccess,
+    links = links.toEntity(),
+    details = details
+)
+
+private fun LaunchEntity.toDomain() = Launch(
+    flightNumber = flightNumber,
+    missionName = missionName,
+    launchYear = launchYear,
+    rocket = rocket.toDomain(),
+    launchSuccess = launchSuccess,
+    links = links.toDomain(),
+    details = details
+)
+
+private fun RocketEntity.toDomain() = Rocket(
+    rocketName =  rocketName,
+    rocketType = rocketType
+)
+
+private fun Rocket.toEntity() = RocketEntity(
+    rocketName =  rocketName,
+    rocketType = rocketType
+)
+
+private fun LinksEntity.toDomain() = Links(
+    missionPatch = missionPatch,
+    missionPatchSmall = missionPatchSmall,
+    article = article,
+    video = video
+)
+
+private fun Links.toEntity() = LinksEntity(
+    missionPatch = missionPatch,
+    missionPatchSmall = missionPatchSmall,
+    article = article,
+    video = video
+)
