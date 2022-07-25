6fb4458d990af6644c8767d5a51fd63dc4f54097
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 20:35:17 -0300

    Changed the onConflict Room annotation and added delete launch function

--- app/src/main/java/com/mvclopes/spacexlaunches/data/datasource/local/dao/SpaceXDao.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/data/datasource/local/dao/SpaceXDao.kt
@@ -1,8 +1,9 @@
 package com.mvclopes.spacexlaunches.data.datasource.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy.ABORT
 import androidx.room.Query
 import com.mvclopes.spacexlaunches.data.datasource.local.model.LaunchEntity
 
@@ -12,13 +13,16 @@ interface SpaceXDao {
     @Query("SELECT * FROM tb_space_x_launches")
     suspend fun getAllLaunches(): List<LaunchEntity>
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = ABORT)
     suspend fun insertAll(launches: List<LaunchEntity>)
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = ABORT)
     suspend fun insertLaunch(launch: LaunchEntity)
 
     @Query("SELECT * FROM tb_space_x_launches WHERE flight_number LIKE :flightNumber")
     suspend fun getSpecificLaunch(flightNumber: Int): LaunchEntity
+
+    @Delete
+    suspend fun deleteLaunch(launch: LaunchEntity)
 
 }
