d95ace547d4c1172aa394e81e324965b944da46a
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 16:47:25 -0300

    Added local datasource's to repository layer

--- app/src/main/java/com/mvclopes/spacexlaunches/data/repository/SpaceXRepositoryImpl.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/data/repository/SpaceXRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.mvclopes.spacexlaunches.data.repository
 
 import com.mvclopes.spacexlaunches.data.datasource.SpaceXDataSource
 import com.mvclopes.spacexlaunches.data.mapper.toDomain
+import com.mvclopes.spacexlaunches.data.mapper.toEntity
 import com.mvclopes.spacexlaunches.domain.model.Launch
 import com.mvclopes.spacexlaunches.domain.repository.SpaceXRepository
 import kotlinx.coroutines.flow.Flow
@@ -11,5 +12,17 @@ class SpaceXRepositoryImpl(private val datasource: SpaceXDataSource): SpaceXRepo
 
     override fun getAllLaunches(): Flow<List<Launch>> {
         return datasource.getAllLaunches().map { it.toDomain() }
+    }
+
+    override fun getFavoriteLaunches(): Flow<List<Launch>> {
+        return datasource.getFavoriteLaunches().map { it.toDomain() }
+    }
+
+    override fun insertAll(launches: List<Launch>): Flow<Unit> {
+        return datasource.insertAll(launches.toEntity())
+    }
+
+    override fun insertLaunch(launch: Launch): Flow<Unit> {
+        return datasource.insertLaunch(launch.toEntity())
     }
 }
--- app/src/main/java/com/mvclopes/spacexlaunches/domain/repository/SpaceXRepository.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/domain/repository/SpaceXRepository.kt
@@ -5,4 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface SpaceXRepository {
     fun getAllLaunches(): Flow<List<Launch>>
+    fun getFavoriteLaunches(): Flow<List<Launch>>
+    fun insertAll(launches: List<Launch>): Flow<Unit>
+    fun insertLaunch(launch: Launch): Flow<Unit>
 }
