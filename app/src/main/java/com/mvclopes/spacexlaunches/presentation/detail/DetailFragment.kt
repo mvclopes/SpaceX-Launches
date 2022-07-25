3a21e1d50f69dfec9d3f69a8e52b06e1007cbc46
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 15:26:38 -0300

    Created initial detail screen layout

--- app/src/main/java/com/mvclopes/spacexlaunches/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/presentation/detail/DetailFragment.kt
@@ -1,14 +1,13 @@
 package com.mvclopes.spacexlaunches.presentation.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.mvclopes.spacexlaunches.R
 import com.mvclopes.spacexlaunches.databinding.FragmentDetailBinding
+import com.mvclopes.spacexlaunches.utils.loadImageWithProgress
 
 class DetailFragment : Fragment() {
 
@@ -19,6 +18,14 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        loadImageWithProgress(binding.missionPatch, args.launch.links.missionPatch)
+        with(binding) {
+            missionName.text = args.launch.missionName
+            launchYear.text = args.launch.launchYear
+            launchSuccess.text = if(args.launch.launchSuccess) "Yes" else "No"
+            rocketName.text = args.launch.rocket.rocketName
+            details.text = args.launch.details.ifEmpty { "Has no details" }
+        }
         return binding.root
     }
 }
