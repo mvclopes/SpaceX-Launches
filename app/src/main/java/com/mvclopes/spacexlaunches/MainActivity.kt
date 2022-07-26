7da0b0f3eed8ca0dc604e0b9427e7082e1eba502
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 23:59:24 -0300

    Added toolbar's in mainActivity

--- app/src/main/java/com/mvclopes/spacexlaunches/MainActivity.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.mvclopes.spacexlaunches
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.mvclopes.spacexlaunches.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.filter_menu_items, menu)
+        return true
     }
 }
