eb82811e86a871552a29fc1f5ba9a34c524d45cc
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-25 23:21:38 -0300

    Added new endpoints to service layer

--- app/src/main/java/com/mvclopes/spacexlaunches/data/service/SpaceXService.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/data/service/SpaceXService.kt
@@ -7,4 +7,10 @@ interface SpaceXService {
 
     @GET("launches")
     suspend fun getAllLaunches(): List<LaunchResponse>
+
+    @GET("launches?launch_year=2020")
+    suspend fun getLastYearLaunches(): List<LaunchResponse>
+
+    @GET("launches?launch_success=true")
+    suspend fun getOnlyLaunchSuccess(): List<LaunchResponse>
 }
