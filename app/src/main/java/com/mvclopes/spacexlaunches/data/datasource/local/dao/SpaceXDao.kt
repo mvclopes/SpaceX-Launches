fd58e609fee61c89c30b12dbfe915d1c5217762d
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-27 11:56:20 -0300

    Punctual change in dao function

--- app/src/main/java/com/mvclopes/spacexlaunches/data/datasource/local/dao/SpaceXDao.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/data/datasource/local/dao/SpaceXDao.kt
@@ -17,7 +17,7 @@ interface SpaceXDao {
     suspend fun insertLaunch(launch: LaunchEntity)
 
     @Query("SELECT * FROM tb_space_x_launches WHERE flight_number LIKE :flightNumber")
-    suspend fun getSpecificLaunch(flightNumber: Int): LaunchEntity
+    suspend fun getSpecificLaunch(flightNumber: Int): LaunchEntity?
 
     @Delete
     suspend fun deleteLaunch(launch: LaunchEntity)
