41248f9b931c0665370666776e68799578a3fa96
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-27 00:49:35 -0300

    Added function to load image with custom progress bar

--- app/src/main/java/com/mvclopes/spacexlaunches/utils/ImageLoader.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/utils/ImageLoader.kt
@@ -1,8 +1,15 @@
 package com.mvclopes.spacexlaunches.utils
 
+import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.mvclopes.spacexlaunches.R
 
 private const val STROKE_WIDTH = 5f
@@ -17,7 +24,7 @@ fun loadImage(view: ImageView, url: String) {
         .into(view)
 }
 
-fun loadImageWithProgress(view: ImageView, url: String) {
+fun loadImageWithCircularProgress(view: ImageView, url: String) {
     val circularProgress = CircularProgressDrawable(view.context)
     circularProgress.strokeWidth = STROKE_WIDTH
     circularProgress.centerRadius = CENTER_RADIUS
@@ -27,6 +34,38 @@ fun loadImageWithProgress(view: ImageView, url: String) {
         .with(view)
         .load(url)
         .placeholder(circularProgress)
+        .error(R.drawable.ic_broken_image)
+        .into(view)
+}
+
+fun loadImageWithCustomProgress(view: ImageView, customProgress: View, url: String) {
+    customProgress.isVisible = true
+    Glide
+        .with(view.context)
+        .load(url)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                customProgress.isVisible = false
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                customProgress.isVisible = false
+                return false
+            }
+
+        })
         .error(R.drawable.ic_broken_image)
         .into(view)
 }
