9130a14db94854405a61d922fed72b61a87ea195
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-26 23:35:15 -0300

    Changed detail layout

--- app/src/main/java/com/mvclopes/spacexlaunches/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/mvclopes/spacexlaunches/presentation/detail/DetailFragment.kt
@@ -6,10 +6,13 @@ import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.mvclopes.spacexlaunches.R
 import com.mvclopes.spacexlaunches.databinding.FragmentDetailBinding
+import com.mvclopes.spacexlaunches.domain.model.Launch
+import com.mvclopes.spacexlaunches.utils.getFormattedString
 import com.mvclopes.spacexlaunches.utils.loadImageWithProgress
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -17,13 +20,14 @@ class DetailFragment : Fragment() {
 
     private val binding: FragmentDetailBinding by lazy { FragmentDetailBinding.inflate(layoutInflater) }
     private val args by navArgs<DetailFragmentArgs>()
+    private val launch: Launch by lazy { args.launch }
     private val viewModel: DetailViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel.isFavoriteLaunch(args.launch)
+        viewModel.isFavoriteLaunch(launch)
         renderContent()
         viewModel.state.observe(viewLifecycleOwner) { state -> setFavoriteIcon(state) }
         setHasOptionsMenu(true)
@@ -35,22 +39,21 @@ class DetailFragment : Fragment() {
     }
 
     private fun renderContent() {
-        loadImageWithProgress(binding.missionPatch, args.launch.links.missionPatch)
+        loadImageWithProgress(binding.missionPatch, launch.links.missionPatch)
         with(binding) {
-            missionName.text = args.launch.missionName
-            launchYear.text = args.launch.launchYear
-            launchSuccess.text = setLaunchSuccessMessage()
-            rocketName.text = args.launch.rocket.rocketName
+            missionName.text = getFormattedString(R.string.mission_name_label, launch.missionName)
+            launchYear.text = getFormattedString(R.string.launch_year_label, launch.launchYear)
+            launchSuccess.text = getFormattedString(R.string.launch_success_label, setLaunchSuccessMessage())
+            rocketName.text = getFormattedString(R.string.rocket_name_label, launch.rocket.rocketName)
             details.text = setDetailsMessage()
-            favoriteIcon.setOnClickListener {
-                viewModel.onFavoriteIconClicked(args.launch)
-            }
+            favoriteIcon.setOnClickListener { viewModel.onFavoriteIconClicked(launch) }
+            configureButtonOpenVideo()
         }
     }
 
-    private fun setDetailsMessage() = args.launch.details.ifEmpty { "Has no details" }
+    private fun setDetailsMessage() = launch.details.ifEmpty { "Has no details" }
 
-    private fun setLaunchSuccessMessage() = if (args.launch.launchSuccess) "Yes" else "No"
+    private fun setLaunchSuccessMessage() = if (launch.launchSuccess) "Yes" else "No"
 
     private fun setFavoriteIcon(state: DetailUiState?) {
         when (state?.isFavorite) {
@@ -62,6 +65,12 @@ class DetailFragment : Fragment() {
                     AppCompatResources
                         .getDrawable(requireContext(), R.drawable.ic_favorite_border)
                 )
+        }
+    }
+
+    private fun configureButtonOpenVideo() {
+        binding.buttonOpenVideo.isVisible = launch.links.video.isNotBlank()
+        binding.buttonOpenVideo.setOnClickListener {
         }
     }
 }
